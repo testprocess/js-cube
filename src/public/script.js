0fbc2ccd43d9278c26d58f576fabeb7cef3fed79
Dipokal HHJ <hhj@devent.kr>
2023-04-27 17:35:58 +0900

    feat: add vector class

--- src/public/script.js
+++ src/public/script.js
@@ -3,6 +3,26 @@ let object = {
     screen: undefined
 }
 
+class Vector {
+    constructor(x, y, z) {
+        this.x = x || 0
+        this.y = y || 0
+        this.z = z || 0
+    }
+
+    dotProduct(vector) {
+        return (this.x * vector.x) + (this.y * vector.y) + (this.z * vector.z) 
+    }
+
+    crossProduct(vector) {
+        return {
+            x: (this.y * vector.z) - (this.z * vector.y), 
+            y: (this.z * vector.x) - (this.x * vector.z),
+            z: (this.x * vector.y) - (this.y * vector.x)
+        }
+    }
+}
+
 class Point {
     constructor(x, y, z) {
         this.x = x
@@ -35,6 +55,8 @@ class Box {
             new Point(this.centerX - this.width, this.centerY + this.height, 0 + this.depth),
         ]
 
+        this.cameraVector = new Vector(this.centerX, this.centerY, -this.depth)
+
         this.edges = [
             [0, 1], [1, 2], [2, 3], [3, 0],
             [4, 5], [5, 6], [6, 7], [7, 4],
@@ -56,11 +78,12 @@ class Box {
             this.rotateX(0.02)
             this.rotateZ(0.02)
             this.draw()
-        }, 40);
+        }, 10);
     }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        const id = Math.floor(Math.random()*16777215).toString(4)
 
         for (const edge of this.edges) {
             this.ctx.beginPath();
@@ -72,13 +95,36 @@ class Box {
         }
         
         for (const face of this.faces) {
-            this.ctx.beginPath()
-            this.ctx.fillStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`
-            this.ctx.moveTo(this.vertices[face[0]].x, this.vertices[face[0]].y)
-            this.ctx.lineTo(this.vertices[face[1]].x, this.vertices[face[1]].y)
-            this.ctx.lineTo(this.vertices[face[2]].x, this.vertices[face[2]].y)
-            this.ctx.lineTo(this.vertices[face[3]].x, this.vertices[face[3]].y)
-            this.ctx.fill()
+            const v1 = [
+                this.vertices[face[0]].x - this.vertices[face[1]].x, 
+                this.vertices[face[0]].y - this.vertices[face[1]].y, 
+                this.vertices[face[0]].z - this.vertices[face[1]].z
+            ]
+
+            const v2 = [
+                this.vertices[face[0]].x - this.vertices[face[2]].x, 
+                this.vertices[face[0]].y - this.vertices[face[2]].y, 
+                this.vertices[face[0]].z - this.vertices[face[2]].z
+            ]
+
+            const vector1 = new Vector(v1[0], v1[1], v1[2])
+            const vector2 = new Vector(v2[0], v2[1], v2[2])
+
+            const cross = vector1.crossProduct(vector2)
+            const dot = this.cameraVector.dotProduct(cross)
+            console.log(id, dot)
+
+            if (dot > 0) {
+                this.ctx.beginPath()
+                this.ctx.fillStyle = `#ffffff` // {Math.floor(Math.random()*16777215).toString(16)}
+                this.ctx.moveTo(this.vertices[face[0]].x, this.vertices[face[0]].y)
+                this.ctx.lineTo(this.vertices[face[1]].x, this.vertices[face[1]].y)
+                this.ctx.lineTo(this.vertices[face[2]].x, this.vertices[face[2]].y)
+                this.ctx.lineTo(this.vertices[face[3]].x, this.vertices[face[3]].y)
+                this.ctx.fill()
+            }
+
+
         }
     }
 
